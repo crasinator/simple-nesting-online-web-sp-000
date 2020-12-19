478e75c44020d6c2e9d6c96f22bf9bd0efaf9dd2
Jordan Reed <jordanreed03@gmail.com>
2020-12-19 22:37:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,9 +77,12 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
- 		}
+     }
+programmer_hash[:yukihiro_matsumoto][:known_for] = 
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]
 }
-programmer_hash[:yukihiro_matsumoto] = :known_for
+    
 end
 
 def changing_alan
